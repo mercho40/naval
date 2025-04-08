fc1d180da3657bbf8ef56eb3bbcc70d3c085868a
mercho40 <107892036+mercho40@users.noreply.github.com>
2025-04-08 10:39:33 -0300

    generar tablero

--- main.py
+++ main.py
@@ -1,7 +1,39 @@
-tablero: List[List[Bool]]
+# Oliver -->
+# Pepo -->
+# Mercho -->
+#
+import random
 
 
-def generarTablero(n):
+def generateBoard(n):
+    board: list[list[bool]] = [[False for _ in range(n)] for _ in range(n)]
     for i in range(n):
         for j in range(n):
-            tablero[i][j] = False
+            if random.random() >= 0.85:
+                board[i][j] = True
+            else:
+                board[i][j] = False
+    return board
+
+
+def printBoard(board):
+    for row in board:
+        for cell in row:
+            if cell:
+                print("X", end=" ")
+            else:
+                print("O", end=" ")
+        print()
+
+
+def terminalState(board):
+    for row in board:
+        for cell in row:
+            if cell:
+                return False
+    return True
+
+
+maxTries = 20
+
+printBoard(generateBoard(10))
